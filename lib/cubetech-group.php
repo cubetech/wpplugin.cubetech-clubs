270f88642dd4efae7c735c9519c3aa2028a3a439
Christoph S. Ackermann <christoph.ackermann@cubetech.ch>
2013-07-30 19:01:16 +0200

    renamed group labels

--- lib/cubetech-group.php
+++ lib/cubetech-group.php
@@ -2,15 +2,15 @@
 function cubetech_clubs_create_taxonomy() {
 	
 	$labels = array(
-		'name'                => __( 'Gruppen'),
-		'singular_name'       => __( 'Gruppe' ),
+		'name'                => __( 'Vereinsgruppen'),
+		'singular_name'       => __( 'Vereinsgruppe' ),
 		'search_items'        => __( 'Gruppen durchsuchen' ),
 		'all_items'           => __( 'Alle Gruppen' ),
-		'edit_item'           => __( 'Gruppe bearbeiten' ), 
-		'update_item'         => __( 'Gruppe aktualisiseren' ),
-		'add_new_item'        => __( 'Neue Gruppe hinzufügen' ),
+		'edit_item'           => __( 'Vereinsgruppe bearbeiten' ), 
+		'update_item'         => __( 'Vereinsgruppe aktualisiseren' ),
+		'add_new_item'        => __( 'Neue Vereinsgruppe hinzufügen' ),
 		'new_item_name'       => __( 'Gruppenname' ),
-		'menu_name'           => __( 'Gruppen' )
+		'menu_name'           => __( 'Vereinsgruppe' )
 	);
 
 	$args = array(
